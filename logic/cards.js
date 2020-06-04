d8c546aa6ea231ebf94e97a8619f05df42f88f40
Tucker R. Twomey <tucker.mentoring@gmail.com>
2020-06-03 23:46:36 -0700

    Add sorting logic.

--- logic/cards.js
+++ logic/cards.js
@@ -1,3 +1,28 @@
+// Numbers from 2 to 10 as strings
+const nums         = [...Array(9).keys()].map((x)=>{return (x+2).toString()});
+// All card values except for ace
+const cardsNotAce  = nums.concat(["jack", "queen", "king"]);
+
+// Completing sort order for aces high and low.
+const cardsAceHigh = cardsNotAce.concat(["ace"]);
+const cardsAceLow  = ["ace"].concat(cardsNotAce);
+
+cardToNumber = (array, card) =>
+{
+  const arrayPair = [...array.keys()].map((i) => {return [array[i], i]});
+  const sortList  = Object.fromEntries(arrayPair);
+  return sortList[card];
+}
+
+module.exports.compare = (aceHigh, c1, c2) =>
+{
+  const sort = aceHigh ? cardsAceHigh : cardsAceLow;
+  const num1 = cardToNumber(sort, c1);
+  const num2 = cardToNumber(sort, c2);
+
+  return (num1 > num2) - (num1 < num2);
+}
+
 module.exports.shuffle = (array) =>
 {
   for(var i=0; i<array.length; i++)
