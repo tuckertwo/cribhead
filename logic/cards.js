d0bca1de52bccef2485b5186e42848043548b926
Tucker R. Twomey <tucker.mentoring@gmail.com>
2020-06-05 11:58:15 -0700

    Clean up shuffle() function.

--- logic/cards.js
+++ logic/cards.js
@@ -55,9 +55,7 @@ module.exports.shuffle = (array) =>
   for(var i=0; i<array.length; i++)
   {
     var rand    = Math.floor(Math.random()*i);
-    var temp    = array[i];
-    array[i]    = array[rand];
-    array[rand] = temp
+    [array[i], array[rand]] = [array[rand], array[i]]
   }
   return array;
 }
